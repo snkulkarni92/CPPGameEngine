a2b03380ff8ed2c8f01f137b417103d108368c57
Saurabh Kulkarni <u0920159@utah.edu>
2015-09-15 16:38:35 -0600

    Decided against changing any code at last second
    Changed the vertex format to match OpenGL

--- Code/Engine/Graphics/Mesh.h
+++ Code/Engine/Graphics/Mesh.h
@@ -24,7 +24,11 @@ namespace eae6320
 			// COLOR0
 			// 4 uint8_ts == 4 bytes
 			// Offset = 8
+#if defined EAE6320_PLATFORM_GL
 			uint8_t r, g, b, a;	// 8 bits [0,255] per RGBA channel (the alpha channel is unused but is present so that color uses a full 4 bytes)
+#elif defined EAE6320_PLATFORM_D3D
+			uint8_t b, g, r, a;	// Direct3D expects the byte layout of a color to be different from what you might expect
+#endif //Platform Check
 		};
 		class Mesh
 		{
